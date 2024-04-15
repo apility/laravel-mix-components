cf76ba1cbb0beff3daaa67e8af38cddf3d9dec3f
Thomas Alrek <thomas@alrek.no>
2024-04-15 15:34:46 +0200

    Add support for specifying what's getting loaded

--- src/View/Components/MixHead.php
+++ src/View/Components/MixHead.php
@@ -3,6 +3,7 @@
 namespace Apility\LaravelMixComponents\View\Components;
 
 use Illuminate\Support\Collection;
+
 class MixHead extends MixBase
 {
   /** @var Collection */
@@ -14,23 +15,30 @@ class MixHead extends MixBase
   /** @var bool */
   public $preload = false;
 
+  public string $type;
+
   /**
    * Create a new component instance.
    *
    * @param string $manifestDirectory
    */
-  public function __construct($manifestDirectory = '', $integrity = null, $crossorigin = null, $preload = false)
+  public function __construct($manifestDirectory = '', $integrity = null, $crossorigin = null, $preload = false, string $type = 'both')
   {
     parent::__construct($manifestDirectory, $integrity, $crossorigin);
 
     $this->preload = $preload;
-    $this->cssFiles = $this->isHot ? collect() : $this->files->get('css', collect());
-    $this->jsFiles = $this->files->get('js', collect());
+    $this->type = $type;
+    $this->cssFiles = in_array($type, ['css', 'both']) ? ($this->isHot ? collect() : $this->files->get('css', collect())) : collect();
+    $this->jsFiles = in_array($type, ['js', 'both']) ? ($this->files->get('js', collect())) : collect();
   }
 
   public function shouldRender()
   {
-    return ($this->cssFiles->count() + $this->jsFiles->count()) > 0;
+    if ($this->type === 'both') {
+      return ($this->cssFiles->count() + $this->jsFiles->count()) > 0;
+    }
+
+    return $this->cssFiles->count() > 0 || $this->jsFiles->count() > 0;
   }
 
   public function render()
